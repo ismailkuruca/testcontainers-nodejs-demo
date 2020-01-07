3a83f4c77e1524ba01e2145c4ca06b54bc0a06a7
Ismail Kuruca <ismail.kuruca@klarna.com>
2020-01-07 09:57:52 +0100

    implement integration tests

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,22 @@
 module.exports = {
   test: {
-    client: 'pg',
-    connection: 'postgres://localhost/postgres',
-    migrations: {
-      directory: __dirname + '/db/migrations'
+    client: "pg",
+    connection: {
+      host: "localhost",
+      user: "test",
+      password: "test",
+      database: "postgres",
+      port: process.env.PG_PORT
     },
+    migrations: {
+      directory: __dirname + "/db/migrations"
+    }
   },
   development: {
-    client: 'pg',
-    connection: 'postgres://postgres:@localhost:32768/postgres',
+    client: "pg",
+    connection: "postgres://postgres:@localhost:32768/postgres",
     migrations: {
-      directory: __dirname + '/db/migrations'
-    },
-  },
-}
+      directory: __dirname + "/db/migrations"
+    }
+  }
+};
--- tests/todos.integration.test.js
+++ tests/todos.integration.test.js
@@ -1,13 +1,47 @@
 const { GenericContainer } = require("testcontainers");
-const request = require('supertest')
-const app = require('../app')
+const request = require("supertest");
 
-describe('Get Todos', () => {
-  it('should return all todos', async () => {
-    const res = await request(app)
-      .get('/api/todos')
-    console.log(res)
-    expect(res.statusCode).toEqual(200)
-    expect(res.body).toHaveProperty('post')
-  })
-})
+jest.setTimeout(20000);
+
+let app;
+let pgContainer;
+let knexTestSetup;
+beforeAll(async () => {
+  pgContainer = await new GenericContainer("postgres")
+    .withEnv("POSTGRES_USER", "test")
+    .withEnv("POSTGRES_PASSWORD", "test")
+    .withEnv("POSTGRES_DB", "postgres")
+    .withExposedPorts(5432)
+    .start();
+  process.env.PG_PORT = pgContainer.getMappedPort(5432);
+  app = require("../app");
+  const knexTestConfig = require("../knexfile.js")["test"];
+  knexTestSetup = require("knex")(knexTestConfig);
+  await knexTestSetup.migrate.latest();
+});
+
+afterAll(async () => {
+  await pgContainer.stop();
+});
+
+describe("Get Todos", () => {
+  beforeEach(async () => {
+    await knexTestSetup.raw("insert into todo_list values(123, '345');");
+    await knexTestSetup.raw("insert into todo_list values(234, '456');");
+  });
+  afterEach(async () => {
+    await knexTestSetup.raw("delete from todo_list");
+  });
+  it("should return all todos", async done => {
+    const res = await request(app).get("/api/todos");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.length).toEqual(2);
+    done();
+  });
+  it("should return specific todo with id", async done => {
+    const res = await request(app).get("/api/todos/234");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual([{ id: 234, creator: "456", todo_items: [] }]);
+    done();
+  });
+});
