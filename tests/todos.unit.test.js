421a428074ceb62b0c2c2127e71af6aa0bfcf7b8
ismailkuruca <ismailkuruca@users.noreply.github.com>
2020-01-10 14:37:24 +0100

    simplify tests

--- tests/todos.unit.test.js
+++ tests/todos.unit.test.js
@@ -1,29 +1,22 @@
 const request = require("supertest");
 let app;
 beforeAll(() => {
-  jest.mock("../middlewares/getAllTodos", () =>
-    jest.fn((req, _res, next) => {
+  jest.mock("../middlewares/getAllTodos", (req, _res, next) => {
       req.getAllTodosResponse = [
         { id: 999, creator: "me" },
         { id: 998, creator: "they" }
       ];
       next();
-    })
-  );
-  jest.mock("../middlewares/getTodoById", () =>
-    jest.fn((req, _res, next) => {
+    });
+  jest.mock("../middlewares/getTodoById", (req, _res, next) => {
       req.getTodoByIdResponse = { id: 998, creator: "456", todo_items: [] };
       next();
-    })
-  );
+    });
   app = require("../app");
 });
 
-afterAll(done => {
-  jest.unmock("../middlewares/getTodoById");
-  jest.unmock("../middlewares/getAllTodos");
+afterAll(() => {
   app.close();
-  done();
 });
 
 describe("Get Todos", () => {
